a490e5dad3bd934bc6fc13ec36725068e40c26a2
Oguzhan Bilgi <oguzhanbilgi35@gmail.com>
2023-06-16 19:37:33 +0300

    aktur-1

---
+++ Aktur-Proje-1/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace Aktur_Proje_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnTumBilgileriGetir = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.labelAd = new System.Windows.Forms.Label();
+            this.labelSoyad = new System.Windows.Forms.Label();
+            this.labelDepartman = new System.Windows.Forms.Label();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.txtDepartman = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.AD = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SOYAD = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DEPARTMAN = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(88, 212);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(173, 34);
+            this.btnEkle.TabIndex = 0;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnTumBilgileriGetir
+            // 
+            this.btnTumBilgileriGetir.Location = new System.Drawing.Point(666, 212);
+            this.btnTumBilgileriGetir.Name = "btnTumBilgileriGetir";
+            this.btnTumBilgileriGetir.Size = new System.Drawing.Size(173, 34);
+            this.btnTumBilgileriGetir.TabIndex = 1;
+            this.btnTumBilgileriGetir.Text = "Tüm Bilgileri Getir";
+            this.btnTumBilgileriGetir.UseVisualStyleBackColor = true;
+            this.btnTumBilgileriGetir.Click += new System.EventHandler(this.btnTumBilgileriGetir_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(281, 212);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(173, 34);
+            this.btnSil.TabIndex = 2;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // labelAd
+            // 
+            this.labelAd.AutoSize = true;
+            this.labelAd.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelAd.Location = new System.Drawing.Point(194, 107);
+            this.labelAd.Name = "labelAd";
+            this.labelAd.Size = new System.Drawing.Size(30, 19);
+            this.labelAd.TabIndex = 3;
+            this.labelAd.Text = "Ad";
+            // 
+            // labelSoyad
+            // 
+            this.labelSoyad.AutoSize = true;
+            this.labelSoyad.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelSoyad.Location = new System.Drawing.Point(189, 137);
+            this.labelSoyad.Name = "labelSoyad";
+            this.labelSoyad.Size = new System.Drawing.Size(56, 19);
+            this.labelSoyad.TabIndex = 4;
+            this.labelSoyad.Text = "Soyad";
+            // 
+            // labelDepartman
+            // 
+            this.labelDepartman.AutoSize = true;
+            this.labelDepartman.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelDepartman.Location = new System.Drawing.Point(166, 161);
+            this.labelDepartman.Name = "labelDepartman";
+            this.labelDepartman.Size = new System.Drawing.Size(95, 19);
+            this.labelDepartman.TabIndex = 5;
+            this.labelDepartman.Text = "Departman";
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(267, 100);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(152, 20);
+            this.txtAd.TabIndex = 6;
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Location = new System.Drawing.Point(267, 130);
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(152, 20);
+            this.txtSoyad.TabIndex = 7;
+            // 
+            // txtDepartman
+            // 
+            this.txtDepartman.Location = new System.Drawing.Point(267, 160);
+            this.txtDepartman.Name = "txtDepartman";
+            this.txtDepartman.Size = new System.Drawing.Size(152, 20);
+            this.txtDepartman.TabIndex = 8;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.AD,
+            this.SOYAD,
+            this.DEPARTMAN});
+            this.dataGridView1.Location = new System.Drawing.Point(234, 288);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(443, 91);
+            this.dataGridView1.TabIndex = 9;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(470, 212);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(173, 34);
+            this.btnUpdate.TabIndex = 10;
+            this.btnUpdate.Text = "Güncelle";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            // 
+            // AD
+            // 
+            this.AD.HeaderText = "AD";
+            this.AD.Name = "AD";
+            // 
+            // SOYAD
+            // 
+            this.SOYAD.HeaderText = "SOYAD";
+            this.SOYAD.Name = "SOYAD";
+            // 
+            // DEPARTMAN
+            // 
+            this.DEPARTMAN.HeaderText = "DEPARTMAN";
+            this.DEPARTMAN.Name = "DEPARTMAN";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 450);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtDepartman);
+            this.Controls.Add(this.txtSoyad);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.labelDepartman);
+            this.Controls.Add(this.labelSoyad);
+            this.Controls.Add(this.labelAd);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnTumBilgileriGetir);
+            this.Controls.Add(this.btnEkle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnTumBilgileriGetir;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Label labelAd;
+        private System.Windows.Forms.Label labelSoyad;
+        private System.Windows.Forms.Label labelDepartman;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.TextBox txtDepartman;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn AD;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SOYAD;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DEPARTMAN;
+    }
+}
+
---
+++ Aktur-Proje-1/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aktur_Proje_1
+{
+    public partial class Form1 : Form
+    {
+        // MSSQL Server veritabanı bağlantı dizesi
+        private string connectionString = ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString;
+        private int selectedRowIndex = -1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnUpdate.Click += btnUpdate_Click;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            // TextBox'lardan girilen değerleri al
+            string ad = txtAd.Text;
+            string soyad = txtSoyad.Text;
+            string departman = txtDepartman.Text;
+
+            // Veritabanına bağlan
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    // Ekleme sorgusu
+                    string query = "INSERT INTO Kullanicilar (Ad, Soyad, Departman) VALUES (@Ad, @Soyad, @Departman)";
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        // Parametreleri ata
+                        command.Parameters.AddWithValue("@Ad", ad);
+                        command.Parameters.AddWithValue("@Soyad", soyad);
+                        command.Parameters.AddWithValue("@Departman", departman);
+
+                        // Sorguyu çalıştır
+                        command.ExecuteNonQuery();
+
+                        MessageBox.Show("Veritabanına kaydedildi.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Hata: " + ex.Message);
+                }
+            }
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            // Silme işlemi için TextBox'lardan girilen değerleri al
+            string ad = txtAd.Text;
+            string soyad = txtSoyad.Text;
+
+            // Veritabanına bağlan
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    // Silme sorgusu
+                    string query = "DELETE FROM Kullanicilar WHERE Ad = @Ad AND Soyad = @Soyad";
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        // Parametreleri ata
+                        command.Parameters.AddWithValue("@Ad", ad);
+                        command.Parameters.AddWithValue("@Soyad", soyad);
+
+                        // Sorguyu çalıştır
+                        int affectedRows = command.ExecuteNonQuery();
+
+                        if (affectedRows > 0)
+                        {
+                            MessageBox.Show("Kayıt silindi.");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Kayıt bulunamadı.");
+                            MessageBox.Show("Silmek için en az ad ve soyad girmelisiniz");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Hata: " + ex.Message);
+                }
+            }
+        }
+
+        private void btnTumBilgileriGetir_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    // Verileri getiren sorgu
+                    string query = "SELECT Id, Ad, Soyad, Departman FROM Kullanicilar";
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            // DataGridView kontrolünü temizle
+                            dataGridView1.Rows.Clear();
+
+                            // Verileri oku ve DataGridView'e ekle
+                            while (reader.Read())
+                            {
+                                int id = reader.GetInt32(0);
+                                string ad = reader.GetString(1);
+                                string soyad = reader.GetString(2);
+                                string departman = reader.GetString(3);
+
+                                // Hücrelere verileri ekle
+                                dataGridView1.Rows.Add(id, ad, soyad, departman);
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Hata: " + ex.Message);
+                }
+            }
+        }
+
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            // Seçili satırın indeksini al
+            int selectedRowIndex = dataGridView1.SelectedCells[0].RowIndex;
+
+            // Güncellenecek değerleri al
+            string ad = txtAd.Text;
+            string soyad = txtSoyad.Text;
+            string departman = txtDepartman.Text;
+
+            // Veritabanına bağlan
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    // Güncelleme sorgusu
+                    string query = "UPDATE Kullanicilar SET Ad = @Ad, Soyad = @Soyad, Departman = @Departman WHERE Id = @SelectedId";
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        // Parametreleri ata
+                        command.Parameters.AddWithValue("@Ad", ad);
+                        command.Parameters.AddWithValue("@Soyad", soyad);
+                        command.Parameters.AddWithValue("@Departman", departman);
+                        string selectedId = dataGridView1.Rows[selectedRowIndex].Cells["Id"].Value.ToString();
+                        command.Parameters.AddWithValue("@SelectedId", selectedId);
+
+                        // Sorguyu çalıştır
+                        int affectedRows = command.ExecuteNonQuery();
+
+                        if (affectedRows > 0)
+                        {
+                            MessageBox.Show("Kayıt güncellendi.");
+
+                            // DataGridView'de güncellenen satırın bilgilerini güncelle
+                            dataGridView1.Rows[selectedRowIndex].Cells["Ad"].Value = ad;
+                            dataGridView1.Rows[selectedRowIndex].Cells["Soyad"].Value = soyad;
+                            dataGridView1.Rows[selectedRowIndex].Cells["Departman"].Value = departman;
+                        }
+                        else
+                        {
+                            MessageBox.Show("Güncelleme işlemi başarısız.");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Hata: " + ex.Message);
+                }
+            }
+        }
+
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Seçili satırın indeksini al
+            int selectedRowIndex = e.RowIndex;
+
+            // Seçili satırın verilerini TextBox'lara doldur
+            if (selectedRowIndex >= 0 && selectedRowIndex < dataGridView1.Rows.Count)
+            {
+                DataGridViewRow selectedRow = dataGridView1.Rows[selectedRowIndex];
+                txtAd.Text = selectedRow.Cells["Ad"].Value.ToString();
+                txtSoyad.Text = selectedRow.Cells["Soyad"].Value.ToString();
+                txtDepartman.Text = selectedRow.Cells["Departman"].Value.ToString();
+            }
+        }
+    }
+}
